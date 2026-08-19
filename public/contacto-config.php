9fb724ad98d44402e9893922c29ab88c0bf16814
Raul r <prueba@correo.com>
2026-08-19 13:53:50 -0400

    Contacto: suma info@tornomatica.cl a los destinatarios

--- public/contacto-config.php
+++ public/contacto-config.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 return [
   'BASE_PATH' => '/prueba',
   'SITE_URL' => 'https://www.tornomatica.cl',
-  'TO_EMAILS_BASE' => '',
+  'TO_EMAILS_BASE' => 'info@tornomatica.cl',
   'TO_EMAIL' => 'codigoraul@gmail.com',
   'FROM_EMAIL' => 'info@tornomatica.cl',
   'FROM_NAME' => 'Tornomatica',
